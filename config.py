04cdd1566d76a0e5f0fea4c5825497bd7780c4a0
cylim76 <cylim76@gmail.com>
2018-07-10 18:23:01 +0800

    Update config.py
    采用confgparser 模块 管理设置信息,多个参数方案 设置信息保存到 conf.ini文件中,

--- config.py
+++ config.py
@@ -5,27 +5,55 @@
 # @Last Modified time: 2018-06-25 17:34:04
 
 
-# 交易类型
-symbols = ['ft', 'usdt']
-# 数量
-amount = 5
-# 深度图买一卖一差值
-price_difference = 0.000004
-# 当前直接购买 （万二的差价，1直接下单）
-is_direct_buy = 0
-# 查询余额类型
-symbol_type = ['usdt', 'ft', 'btc']
-# 买卖间隔时间
-second = 1
+import configparser
 
+#多个设置的时候 每次启动程序时输入参数section, 调用conf.ini中的对应设置
+# section = input('请输入section') or 'section'
+section = 'section'
+
+
+conf = configparser.ConfigParser()
+conf.read('conf.ini')
+
+api_key = conf.get(section, "key")
+api_secret = conf.get(section, "secret")
+
+# 交易类型
+symbols = list(conf.get(section, "symbols").replace(' ', '').split(','))
+
+# 交易对,查询交易明细用
+symbol_couple = list(conf.get(section,"symbol_couple").replace(' ', '').split(','))
+
+# 数量
+amount = int(conf.get(section, "amount"))
+
+# 深度图买一卖一差值
+price_difference = float(conf.get(section, "price_difference"))
+
+# 当前直接购买 （万二的差价，1直接下单）
+is_direct_buy = int(conf.get(section, "is_direct_buy"))
+
+# 查询余额类型
+# symbol_type = ['usdt', 'ft', 'btc']
+symbol_type = list(conf.get(section, "symbol_type").replace(' ', '').split(','))
+
+# 买卖间隔时间
+second = int(conf.get(section, "second"))
 
 # 需要计算手续费的开始时间
-fees_start_time = {
-	'year': 2018,
-	'month': 6,
-	'day': 20,
-	'hour': 0,
-	'minute': 0,
-	'second': 0
-}
+fees_start_time = dict(zip(['year', 'month', 'day', 'hour', 'minute', 'second'], map(int, list(conf.get(section,"fees_start_time").split(' ')))))
 
+print('当前参数设置如下: ')
+print('下单数量(amount) : ', amount)
+print('交易对(symbols) : ', symbols)
+print('价格差(price_difference) : {:<14.8f}'.format(price_difference))
+print('下单间隔秒(second) : ', second)
+
+if __name__ == '__main__':
+    print('apikey : ', api_key, '\napi_secret: ', api_secret)
+    print('下单数量(amount) : ', amount)
+    print('交易对(symbols) : ', symbols)
+    print('价格差(price_difference) : ', price_difference)
+    print('查询余额类型(symbol_types) : ', symbol_type)
+    print('下单间隔秒(second) : ', second)
+    print('fees_start_time : ', fees_start_time, type(fees_start_time))
