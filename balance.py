b9000c9b75eceb3688af95af23271762aa923d7a
cylim76 <cylim76@gmail.com>
2018-07-10 18:04:44 +0800

    Update balance.py
    资产查询结果 格式化输出

--- balance.py
+++ balance.py
@@ -13,13 +13,36 @@ fcoin = Fcoin(api_key, api_secret)
 
 # 查询账户余额
 def get_balance_action(symbols):
-    balance_info = fcoin.get_balance()
-    print(balance_info)
-    for info in balance_info['data']:
-    	for symbol in symbols:
-	        if info['currency'] == symbol:
-	            balance = info
-	            print(balance['currency'], '账户余额', balance['balance'], '可用', balance['available'], '冻结', balance['frozen'])
+#     balance_info = fcoin.get_balance()
+#     print(balance_info)
+#     for info in balance_info['data']:
+#     	for symbol in symbols:
+# 	        if info['currency'] == symbol:
+# 	            balance = info
+# 	            print(balance['currency'], '账户余额', balance['balance'], '可用', balance['available'], '冻结', balance['frozen'])
+    mingxi = fcoin.get_balance()['data']
+    mx_sort = []
+
+    for i in range(len(mingxi)):
+        mx_sort.append(mingxi[i]['currency'])
+    mx_sort.sort()
+
+    print('_' * 55)
+    print('|{:^8}|{:^14}|{:^14}|{:^14}|'.format('currency', 'balance', 'frozen', 'available'))
+    print('-' * 55)
+    for k in range(len(mx_sort)):
+        for i in mingxi:
+            if i['currency'] == mx_sort[k]:
+                print('|{:^8}|{:>14.4f}|{:>14.4f}|{:>14.4f}|'.format(i['currency'].upper(),
+                                                                     float(i['balance']),
+                                                                     float(i['frozen']),
+                                                                     float(i['available'])))
+    print('-' * 55)
+
+
+
+
+
 
 def balance():
     # 账户余额
