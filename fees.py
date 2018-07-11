fbba71c20d7981d1428d0a59fd92248a83ced35c
cylim76 <cylim76@gmail.com>
2018-07-11 08:33:17 +0800

    Update fees.py
    在conf.ini 设置多个交易对, 可以一次查询多个
    
     部分成交的订单和取消的订单暂时不能查询
    
    .....
    |  14 | FTUSDT |      4.00|  0.24245700|20180710 153646 |limit |sell|    4.00|    0.97020000|  0.000970| api  | filled |
    |  15 | FIUSDT |      3.00|  0.24000000|20180711 082122 |limit |sell|    3.00|    0.72000000|  0.000720| web  | filled |
    ------------------------------------------------------------------------------------------------------------------------
    ------------------------------------------------------------------------------------------------------------------------
    ftusdt:
    卖出金额 : 7.78       		卖出数量 : 33.00      		卖出均价 : 0.235753      		手续费 :  0.01     
    买入金额 : 0.00       		买入数量 : 0.00       		        买入均价 : 0.000000      		手续费 :  0.00     
    ------------------------------------------------------------------------------------------------------------------------
    fiusdt:
    卖出金额 : 7810.03    		卖出数量 : 24006.00   		卖出均价 : 0.325337      		手续费 :  7.81     
    买入金额 : 0.00       		        买入数量 : 0.00       		        买入均价 : 0.000000      		手续费 :  0.00     
    ------------------------------------------------------------------------------------------------------------------------

--- fees.py
+++ fees.py
@@ -6,71 +6,56 @@
 
 from fcoin import Fcoin
 # from auth import api_key, api_secret
-from config import symbols, fees_start_time, api_key, api_secret
+from config import symbols, fees_start_time, api_key, api_secret, symbol_couple
 from datetime import datetime
 import time
 
 # 初始化
 fcoin = Fcoin(api_key, api_secret)
-symbol = symbols[0] + symbols[1]
-symbol_0_fees = 0 
-symbol_1_fees = 0
-
-count = 1
-
-fees = 0
-buy_count = 0
-sell_count = 0
-
-ordermx = [] #  保存下载订单明细的列表
-#保存交易记录统计的字典
-feecacul = [{'symbol':'ftusdt', 'sellcount': 0, 'sellqty': 0, 'sellamt': 0, 'sellfee': 0, 'buycount': 0, 'buyqty': 0, 'buyamt': 0, 'buyfee': 0}]
+# symbol = symbols[0] + symbols[1]   #symbol 改为通过 config.py 中的  symbol_couple 迭代.
 
 
-def fees(after = None, state = 'filled'):
-    global symbol_0_fees, symbol_1_fees, count, buy_count, sell_count, ordermx, dt
+tradelist = []  # 保存下载订单明细的列表变量
+
+tradecount = []  # 保存交易记录统计信息
+
+tradecount_symbol = []  # 统计用出来的交易对
+
+
+def fees(after=None, state='filled'):
+    global count, tradelist
 
     if after:
-        order_list = fcoin.list_orders(symbol = symbol, states = state, after = after)
+        order_list = fcoin.list_orders(symbol=symbol, states=state, after=after)
     else:
-        dt = datetime(fees_start_time['year'], fees_start_time['month'], fees_start_time['day'], fees_start_time['hour'], fees_start_time['minute'], fees_start_time['second'])
+        dt = datetime(fees_start_time['year'], fees_start_time['month'], fees_start_time['day'],
+                      fees_start_time['hour'], fees_start_time['minute'], fees_start_time['second'])
         timestamp = int(dt.timestamp() * 1000)
-        order_list = fcoin.list_orders(symbol = symbol, states = state, after = timestamp)
+        order_list = fcoin.list_orders(symbol=symbol, states=state, after=timestamp)
 
-    # 原来的接收数据直接打印的模块, 改为在这里只保存到 ordermx 列表中, 打印功能移动到reportprint() 函数中
-    # ordermx = order_list['data']
+    # 接收交易明细保存到 tradelist 变量中
     for i in range(len(order_list['data'])):
-        ordermx.append(order_list['data'][i])
-    # for order in order_list['data']:
-    #     strcount = '%4d.'%(count)
-    #     formatstr = '{:.9f}'
-    #     print(strcount, '挂单价格', formatstr.format(float(order['price'])), '成交数量', formatstr.format(float(order['filled_amount'])), '方向', order['side'])
-    #     if order['side'] == 'sell':
-    #         sell_count += 1
-    #         symbol_1_fees += float(order['fill_fees'])
-    #     else:
-    #         buy_count += 1
-    #         symbol_0_fees += float(order['fill_fees'])
-    #
-    #     count += 1
-
-
-
+        tradelist.append(order_list['data'][i])
 
     time.sleep(2)
     if len(order_list['data']) == 100:
         fees(order_list['data'][0]['created_at'])
 
-# 数据接并保存到ordermx中. 中print_report 中实现输出报表
+
+# 输出报表
 def print_report():
-    global symbol_0_fees, symbol_1_fees, count, buy_count, sell_count, feecacul
-    fees()
+    global count, tradecount, symbol
+    count = 1
+    # 多个交易对 挨个都查询交易记录,交易对symbol_couple信息在 conf.ini 设置
+    for symbol in symbol_couple:
+        fees()
+
     mx_sort = []
-    for i in range(len(ordermx)):
-        mx_sort.append(ordermx[i]['created_at'])
-    mx_sort.sort()
+    for i in range(len(tradelist)):
+        mx_sort.append(tradelist[i]['created_at'])
+    mx_sort.sort()  # 按时间顺序排序,正序输出-- 字典为元素的列表如何用字典的某个值排序??
     # 打印明细报表
-    print('-' * 117)
+    print('-' * 120)
     print(
         '|{:^5}|{:^8}|{:^10}|{:^12}|{:^16}|{:^6}|{:^4}|{:>8}|{:^14}|{:^10}|{:^6}|{:^8}|'.format('No.', 'symbol'.upper(),
                                                                                                 'qty', 'price',
@@ -79,10 +64,10 @@ def print_report():
                                                                                                 'executed_value',
                                                                                                 'fill_fees', 'source',
                                                                                                 'state'))
-    print('-' * 117)
+    print('-' * 120)
     for k in range(len(mx_sort)):
         strcount = '|{:>4d}'.format(count)
-        for order in ordermx:
+        for order in tradelist:
             if order['created_at'] == mx_sort[k]:
                 print(strcount,
                       '|{:^8}|{:>10.2f}|{:>12.8f}|{:^16}|{:^6}|{:^4}|{:>8.2f}|{:>14.8f}|{:>10.6f}|{:^6}|{:^8}|'.
@@ -94,49 +79,49 @@ def print_report():
 
                 count += 1
 
-    print('-' * 117)
+    print('-' * 120)
 
-    # 统计数据, 从下载来的 ordermx  统计并保存结果到 feecacul 字典变量中
-    for order in ordermx:
-        for feercount in feecacul:
-            n = 1
-            if feercount['symbol'] == order['symbol']:
-                # 已有的加法处理
-                if order['side'] == 'sell':
-                    feercount['sellcount'] += 1  # 交易笔数
-                    feercount['sellqty'] += float(order['filled_amount'])  # 交易数量
-                    feercount['sellamt'] += float(order['executed_value'])  # 交易金额
-                    feercount['sellfee'] += float(order['fill_fees'])  # 产生的手续费
+    # 统计数据, 从下载来的 tradelist  统计并保存汇总结果到 tradecount 字典变量中
+
+    for tradelist_line in tradelist:
+
+        # 第一次出现的交易对, 添加字典到 tradecount列表中
+        if tradelist_line['symbol'] not in tradecount_symbol:
+            if tradelist_line['side'] == 'sell':
+                tradecount.append({'symbol': tradelist_line['symbol'], 'sellcount': 1,
+                                   'sellqty': float(tradelist_line['filled_amount']),
+                                   'sellamt': float(tradelist_line['executed_value']),
+                                   'sellfee': float(tradelist_line['fill_fees']),
+                                   'buycount': 0, 'buyqty': 0, 'buyamt': 0, 'buyfee': 0})
+            else:
+                tradecount.append(
+                    {'symbol': tradelist_line['symbol'], 'sellcount': 0, 'sellqty': 0, 'sellamt': 0,
+                     'sellfee': 0,
+                     'buycount': 1, 'buyqty': float(tradelist_line['filled_amount']),
+                     'buyamt': float(tradelist_line['executed_value']), 'buyfee': float(tradelist_line['fill_fees'])})
+
+            tradecount_symbol.append(tradelist_line['symbol'])
+
+        # 已有的加法处理
+        for tradecount_line in tradecount:
+            if tradecount_line['symbol'] == tradelist_line['symbol']:
+                if tradelist_line['side'] == 'sell':
+                    tradecount_line['sellcount'] += 1  # 交易笔数
+                    tradecount_line['sellqty'] += float(tradelist_line['filled_amount'])  # 交易数量
+                    tradecount_line['sellamt'] += float(tradelist_line['executed_value'])  # 交易金额
+                    tradecount_line['sellfee'] += float(tradelist_line['fill_fees'])  # 产生的手续费
                 else:
                     # buy_count += 1
-                    # symbol_0_fees += float(order['fill_fees'])
-                    feercount['buycount'] += 1
-                    feercount['buyqty'] += float(order['filled_amount'])
-                    feercount['buyamt'] += float(order['executed_value'])
-                    feercount['buyfee'] += float(order['fill_fees'])
-                # print(feecacul)
-                continue
-            if n == len(feecacul):
-                # 没有的 添加字典to list
-                if order['side'] == 'sell':
-                    feecacul.append({'symbol': order['symbol'], 'sellcount': 1,
-                                     'sellqty': float(order['filled_amount']),
-                                     'sellamt': float(order['executed_value']),
-                                     'sellfee': float(order['fill_fees']),
-                                     'buycount': 0, 'buyqty': 0, 'buyamt': 0, 'buyfee': 0})
-                else:
-                    feecacul.append(
-                        {'symbol': order['symbol'], 'sellcount': 0, 'sellqty': 0, 'sellamt': 0,
-                         'sellfee': 0,
-                         'buycount': 1, 'buyqty': float(order['filled_amount']),
-                         'buyamt': float(order['executed_value']), 'buyfee': float(order['fill_fees'])})
-            n += 1
-
-
+                    # symbol_0_fees += float(trade['fill_fees'])
+                    tradecount_line['buycount'] += 1
+                    tradecount_line['buyqty'] += float(tradelist_line['filled_amount'])
+                    tradecount_line['buyamt'] += float(tradelist_line['executed_value'])
+                    tradecount_line['buyfee'] += float(tradelist_line['fill_fees'])
 
     # 汇总结果, 交易量,金额,均价,手续费
-    for data in feecacul:
-        print('-' * 117)
+    # for symbol in tradecount_symbol:
+    for data in tradecount:
+        print('-' * 120)
         print('{}:'.format(data['symbol']))
         if data['sellqty'] > 0:
             sellavg = data['sellamt'] / data['sellqty']
@@ -153,13 +138,9 @@ def print_report():
         print('买入金额 : {:<10.2f} \t\t买入数量 : {:<10.2f} \t\t买入均价 : {:<14.6f}\t\t手续费 :  {:<8.2f} '
               .format(data['buyamt'], data['buyqty'], buyavg, data['buyfee']))
 
-    print('-' * 117)
+    print('-' * 120)
+
 
 if __name__ == '__main__':
     print('正在计算中，请耐心等待...')
-    # fees()
-    # time.sleep(2)
-    # fees(None, 'canceled')
-    # print('当前手续费:', symbols[0], ':', symbol_0_fees, symbols[1], symbol_1_fees)
-    # print('买入', buy_count, '卖出', sell_count)
     print_report()
